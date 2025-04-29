db04ba1446536a1e3502f3cb45c1e0d9aa514a71
Massimiliano Giovagnoli <maxgio92@pm.me>
2025-04-29 20:03:57 +0200

    chore(internal/output): remove feed buffer utilization from stsbar
    Signed-off-by: Massimiliano Giovagnoli <maxgio92@pm.me>

--- internal/output/status.go
+++ internal/output/status.go
@@ -21,10 +21,9 @@ func StatusBar(ctx context.Context, refreshRate time.Duration, printF func()) {
 }
 
 func PrettyTraceStatus(cov float64, rate uint64, evtUtil, feedUtil int) string {
-	return fmt.Sprintf("\r%-50s %-20s %-20s %-20s",
+	return fmt.Sprintf("\r%-50s %-20s %-20s",
 		fmt.Sprintf("Coverage by functions: [%s] %6.2f%%", ProgressBar(int(cov), 40), cov),
 		fmt.Sprintf("Events/s: %4d", rate),
 		fmt.Sprintf("Events Buffer: [%s] %3d%%", ProgressBar(evtUtil, 10), evtUtil),
-		fmt.Sprintf("Feed Buffer: [%s] %3d%%", ProgressBar(feedUtil, 10), feedUtil),
 	)
 }
